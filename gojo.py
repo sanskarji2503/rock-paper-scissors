f58fd37d6dd99ca1c9ad839e710c07853be444eb
sanskar sahu <125204886+sanskarji2503@users.noreply.github.com>
2023-09-02 18:25:25 +0530

    Add files via upload

---
+++ gojo.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[26]:
+
+
+import random
+
+def get_choices():
+    player_choice = input("Enter a choice (rock, paper, scissors) ")
+    options = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(options)
+    choices = {"player": player_choice, "computer": computer_choice}
+    return choices
+
+def check_win(player , computer):
+    print(f"you chose {player} , computer chose {computer}")
+    if player == computer:
+        return "tie!"
+    elif player == "rock":
+        if computer == "scisors":
+            return "rock smashes scissors! you win!"
+        else:
+              return "paper cover rock! you lose."
+    elif player == "paper": 
+            if computer == "rock":
+                return "paper covers rock! you win!"
+            else:
+                return "scissors cuts paper! you lose."
+    elif player == "scissors": 
+            if computer == "paper":
+                 return "scissors cuts paper! you win!"
+            else:
+                 return "rock smashes scissors! you lose."  
+
+choices = get_choices()
+result = check_win(choices["player"] , choices["computer"])
+print(result)
+
